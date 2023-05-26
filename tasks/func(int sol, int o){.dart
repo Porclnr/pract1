b9f445fb17701e0a66d06ac9b4512ee2341780d9
kodelosun <133762837+Porclnr@users.noreply.github.com>
2023-05-27 01:02:23 +0300

    Add files via upload

---
+++ tasks/func(int sol, int o){.dart
@@ -0,0 +1,25 @@
+func(int sol, int o) {
+  int sol;
+  (731674765);
+  assert(('731674765') == 74765, 'rabotaet');
+}
+
+// // func1(int duplicateCount, int o){
+//   int duplicateCount(2);
+//     assert(duplicateCount("aaBbcde")==2);
+// }
+
+void main() {
+  print('третье задание:');
+  var decimal = int.parse('1001', radix: 2);
+  print(decimal);
+
+  print('первое задание:');
+
+  print(func(731674765, 74765));
+
+  print('второе задание');
+
+//   var a = 'aaBcde';
+//   count()
+}
